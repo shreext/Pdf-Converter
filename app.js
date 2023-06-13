6a9b970fb20ec26bcacefe6eb278c7924f7f1ee8
Anant Shree <69853234+anantshree1@users.noreply.github.com>
2023-06-14 05:01:13 +0530

    Api2Pdf

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-console.log("Hello World");
+console.log("Convert2PDF");
 
+// Module Imported
 const express = require('express')
 const path = require('path')
-const puppeteer = require('puppeteer');
 const validUrl = require('valid-url');
 const bodyParser = require("body-parser");
 const pug = require('pug');
 const fs = require('fs');
-
+var Api2Pdf = require('api2pdf');
+var a2pClient = new Api2Pdf('42ad5101-43a6-448f-8c3e-a29b1733984b');
+const download = require('download')
 const app = express()
 const port = 3000;
 
@@ -18,75 +20,70 @@ app.use('/static', express.static('static'))
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// Setting View
 app.get('/', (req, res) => {
   const con = "This is the best content on the internet so far so use it wisely"
   const params = { 'title': 'PubG is the best game', "content": con }
   res.status(200).render('index.pug', params);
-  // res.sendFile(path.join(__dirname, "/index.html"))
 });
 
+// Privacy Policy Page
 app.get('/policy', (req, res) => {
   const con = "This is the best content on the internet so far so use it wisely"
   const params = { 'title': 'PubG is the best game', "content": con }
   res.status(200).render('policy.pug', params);
 });
 
+// Terms and Condition Page
 app.get('/terms', (req, res) => {
   const con = "This is the best content on the internet so far so use it wisely"
   const params = { 'title': 'PubG is the best game', "content": con }
   res.status(200).render('terms.pug', params);
 });
 
+// Disclaimer Page
 app.get('/disclaimer', (req, res) => {
   const con = "This is the best content on the internet so far so use it wisely"
   const params = { 'title': 'PubG is the best game', "content": con }
   res.status(200).render('disclaimer.pug', params);
 });
 
-
-
+// Download Method
 app.post('/download', (req, res) => {
   let urlText = req.body.inputUrl;
   console.log(urlText);
   let d = new Date().getTime();
   let downloadPath = `download${d}.pdf`;
 
+  // Checking Url
   if (urlText != " ") {
-
     if (validUrl.isUri(urlText)) {
-      (async () => {
-        const browser = await puppeteer.launch({ headless: false, args: ["--no-sandbox"] });
-
-        const page = await browser.newPage();
-
-        const website_url = urlText;
-
-        await page.goto(website_url, { timeout: 0 });
-        await page.emulateMediaType('screen');
-
-        let pdfC = await page.pdf({
-          path: `${downloadPath}`,
-          margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-          printBackground: true,
-          format: 'A4',
-        });
-
-        res.download(downloadPath, (error) => {
-          if (!error) {
-            console.log(`File downloaded ${downloadPath}`);
-            console.log(path.join(__dirname, `/${downloadPath}`));
-          } else {
-            console.log("File not downloaded");
-          }
+      // Getting Pdf
+      var options = { orientation: 'potrait', pageSize: 'A4' };
+      a2pClient.wkUrlToPdf(urlText,
+        {
+          inline: false, filename: `${downloadPath}`
         })
+        .then(function (result) {
 
-        await browser.close();
-      })();
+          console.log(result);
+          var responseId = result.ResponseId
+          var filename = result.FileUrl
+
+          console.log(filename);
+          // Downloading Pdf
+          (async () => {
+            await download(filename, './')
+            console.log(`File downloaded ${filename}`);
+
+          })()
+        }, function (rejected) {
+          console.log(rejected); //an error occurred
+        });
     }
     else {
       console.log("Not a Valid Url");
     }
-
   }
   else {
     res.off;
@@ -94,8 +91,5 @@ app.post('/download', (req, res) => {
   }
 })
 
-app.listen(process.env.PORT || port, ()=>console.log(`Listening on port http://localhost:${port}`))
-
-// app.listen(port, () => {
-//   console.log(`Listening on port http://localhost:${port}`);
-// })
+// Listening Port
+app.listen(process.env.PORT || port, () => console.log(`Listening on port http://localhost:${port}`))
